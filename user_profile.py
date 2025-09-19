281803f24ab0f827f078a20f098b2048d132250c
slmmubita-web <smlmubita@gmail.com>
2025-09-19 06:08:06 +0200

    Day 7: python input andformatted output

---
+++ user_profile.py
@@ -0,0 +1,13 @@
+def main():
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    fav_lang = input("Enter your favourite programming language: ")
+    hobby = input("Enter your hobby: ")
+
+    print()
+    print(f"Hello {name}!")
+    print(f"You are: {age}.")
+    print(f"Your favorite programming language is {fav_lang} and your hobby is {hobby}.")
+if __name__ == "__main__":
+    main()
+py
